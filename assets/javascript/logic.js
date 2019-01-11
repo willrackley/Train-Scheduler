44496a3b1d0e77e1e6f1ba9c5f55beebfbaf03d4
williamrackley <willrackley65@gmail.com>
2019-01-11 18:13:52 -0500

    completed taks and fixed issue with editting train info

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,19 +1,20 @@
 var config = {
-    apiKey: "AIzaSyAiNa0JZmuxuCLnP8GxDvDjnTjeLyp_3yI",
-    authDomain: "train-scheduler-bootcamp.firebaseapp.com",
-    databaseURL: "https://train-scheduler-bootcamp.firebaseio.com",
-    projectId: "train-scheduler-bootcamp",
-    storageBucket: "train-scheduler-bootcamp.appspot.com",
-    messagingSenderId: "793449061315"
-  };
-  firebase.initializeApp(config);
+     apiKey: "AIzaSyAiNa0JZmuxuCLnP8GxDvDjnTjeLyp_3yI",
+     authDomain: "train-scheduler-bootcamp.firebaseapp.com",
+     databaseURL: "https://train-scheduler-bootcamp.firebaseio.com",
+     projectId: "train-scheduler-bootcamp",
+     storageBucket: "train-scheduler-bootcamp.appspot.com",
+     messagingSenderId: "793449061315"
+   };
+   firebase.initializeApp(config);
+
+   
 
   var database = firebase.database();
-  var rowCtr = -1;
-  var deleteButtonCtr = -1;
-  var entryArray = [];
+ 
   $(".editForm").hide();
 
+//submit button in the add train form that adds the train information to the database
   $("#submitButton").on("click", function() {
     event.preventDefault();
 
@@ -24,10 +25,8 @@ var config = {
     
  
     if(name === "" || $("#destination").val() === "" || $("#firstTrainTime").val() === "" || $("#frequency").val() === "") {
-        alert("Please complete all input fields.")
-        console.log(rowCtr)
+        alert("Please complete all input fields.");
     } else {
-
     database.ref().push({
       name: name,
       destination: destination,
@@ -55,8 +54,6 @@ database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
    var key = snapshot.key;
    var editKey = snapshot.key;
    
-   rowCtr++;
-   deleteButtonCtr++;
 
    // to find the next arrival time and mins to arrival you cave to find the difference between the current time and first train time
    //then take that number and find the remainder of that number and the frequency
@@ -77,12 +74,11 @@ database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
        row.appendTo("#tableBody");
        $("<td>"+"<button class=deleteButton databaseKey="+ key+">"+"x"+"</button>"+"</td>").appendTo(row);
        $("<td>"+name+"</td>").appendTo(row);
-       $("<td>"+destination+"</td>").appendTo(row);
-       $("<td>"+frequency+"</td>").appendTo(row);
-       $("<td>"+actualArrival+"</td>").appendTo(row);
-       $("<td>"+minsTillArrival+"</td>").appendTo(row);
-       option.appendTo("#dropdownName");
-       option.text(name);
+       $("<td id="+"destinationData"+key+">"+destination+"</td>").appendTo(row);
+       $("<td id="+"frequencyData"+ key+">"+frequency+"</td>").appendTo(row);
+       $("<td id="+"actualArrivalData"+key+">"+actualArrival+"</td>").appendTo(row);
+       $("<td id="+"minsTillArrivalData"+key+">"+minsTillArrival+"</td>").appendTo(row);
+       $("<td>"+"<button class=editButton databaseKey="+ key+">"+"edit"+"</button>"+"</td>").appendTo(row);
        
    } else {
         row.appendTo("#tableBody");
@@ -93,85 +89,77 @@ database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
         $("<td id="+"actualArrivalData"+key+">"+actualArrival+"</td>").appendTo(row);
         $("<td id="+"minsTillArrivalData"+key+">"+minsTillArrival+"</td>").appendTo(row);
         $("<td>"+"<button class=editButton databaseKey="+ key+">"+"edit"+"</button>"+"</td>").appendTo(row);
-        option.appendTo("#dropdownName");
-        option.text(name);
-        
    }
+
+   //function for doing the train math after its editted
+   function trainMath(edittedFirstTrainTime, edittedFrequency, editButtonKey){
+
+     if(edittedFrequency === ""){
+          edittedFrequency = frequency;
+          database.ref().child(editButtonKey).update({frequency: edittedFrequency});
+     }if(edittedFirstTrainTime === ""){
+          edittedFirstTrainTime = firstTrainTime;
+          database.ref().child(editButtonKey).update({firstTrainTime: edittedFirstTrainTime});
+     }
+     
+     var diffOfCurrentAndFirstTime = moment().diff(moment(edittedFirstTrainTime, "HH:mm"), "minutes");
+     var timeDiffandFreqRemainder = diffOfCurrentAndFirstTime % edittedFrequency;
+     var minsTillArrival = edittedFrequency - timeDiffandFreqRemainder;
+     var actualArrival = moment().add(minsTillArrival, "minutes").format("HH:mm");
+     database.ref().child(editButtonKey).update({firstTrainTime: edittedFirstTrainTime});
+     database.ref().child(editButtonKey).update({frequency: edittedFrequency});
+
+     if(moment(edittedFirstTrainTime, "HH:mm") > moment()){
+          actualArrival = moment(edittedFirstTrainTime, "HH:mm").format("HH:mm");
+          minsTillArrival = moment(edittedFirstTrainTime, "HH:mm").diff(moment(), "minutes");
+     } 
+
+     $("#actualArrivalData"+editButtonKey).text(actualArrival);
+     $("#minsTillArrivalData"+editButtonKey).text(minsTillArrival);
+     $("#frequencyData"+editButtonKey).text(edittedFrequency);
+     }
   
+   
    //deletes item from database and the DOM
-   $(".deleteButton").unbind("click").on("click", function(){
-        var key = $(this).attr("databaseKey");
-        $("."+key).remove();
-        database.ref().child(key).remove();
-   });
+     $(".deleteButton").unbind("click").on("click", function(){
+          var key = $(this).attr("databaseKey");
+          $("."+key).remove();
+          database.ref().child(key).remove();
+     });
   
-   $(".editButton").unbind("click").on("click", function(){
-       $("#editSubmitButton").removeAttr("editSubmitKey");
-       $(".editForm").show();
-       var editButtonKey = $(this).attr("databaseKey")
-      
-       $("#editSubmitButton").attr("editSubmitKey",editButtonKey);
+     //opens the form that allows user to edit train info
+     $(".editButton").unbind("click").on("click", function(){
+          $("#editSubmitButton").removeAttr("editSubmitKey");
+          $(".editForm").show();
+          $("#editDestination").val("");
+          $("#editFirstTrainTime").val("");
+          $("#editFrequency").val("");
+          var editButtonKey = $(this).attr("databaseKey")
+          $("#editSubmitButton").attr("editSubmitKey",editButtonKey);
 
-       //this submit button edits the train information
-       $("#editSubmitButton").unbind("click").on("click", function(){
-        var edittedDestination = $("#editDestination").val().trim();
-        var edittedFirstTrainTime = $("#editFirstTrainTime").val().trim();
-        var edittedFrequency = $("#editFrequency").val().trim();
+     //this submit button edits the train information
+     $("#editSubmitButton").unbind("click").on("click", function(){
+          var edittedDestination = $("#editDestination").val().trim();
+          var edittedFirstTrainTime = $("#editFirstTrainTime").val().trim();
+          var edittedFrequency = $("#editFrequency").val().trim();
 
-        var diffOfCurrentAndFirstTime = moment().diff(moment(edittedFirstTrainTime, "HH:mm"), "minutes");
-        var timeDiffandFreqRemainder = diffOfCurrentAndFirstTime % edittedFrequency;
-        var minsTillArrival = edittedFrequency - timeDiffandFreqRemainder;
-        var actualArrival = moment().add(minsTillArrival, "minutes").format("HH:mm");
-    
-        
-       if(edittedDestination !== ""){
-            database.ref().child(editButtonKey).update({destination: edittedDestination});
-            $("#destinationData"+editButtonKey).text(edittedDestination);
-       }
+          trainMath(edittedFirstTrainTime, edittedFrequency, editButtonKey);
 
-       if(edittedFirstTrainTime !== "" && edittedFrequency === ""){
-            database.ref().child(editButtonKey).update({firstTrainTime: edittedFirstTrainTime});
-            edittedFrequency = frequency;
-            
-            timeDiffandFreqRemainder = diffOfCurrentAndFirstTime % edittedFrequency;
-            minsTillArrival = edittedFrequency - timeDiffandFreqRemainder;
-            actualArrival = moment().add(minsTillArrival, "minutes").format("HH:mm");
-            $("#actualArrivalData"+editButtonKey).text(actualArrival);
-            $("#minsTillArrivalData"+editButtonKey).text(minsTillArrival);
-       }
-       if(edittedFrequency !== "" && edittedFirstTrainTime === ""){
-            database.ref().child(editButtonKey).update({frequency: edittedFrequency});
-            edittedFirstTrainTime = firstTrainTime;
-            diffOfCurrentAndFirstTime = moment().diff(moment(edittedFirstTrainTime, "HH:mm"), "minutes");
-            timeDiffandFreqRemainder = diffOfCurrentAndFirstTime % edittedFrequency;
-            minsTillArrival = edittedFrequency - timeDiffandFreqRemainder;
-            actualArrival = moment().add(minsTillArrival, "minutes").format("HH:mm");
-            $("#frequencyData"+editButtonKey).text(edittedFrequency);
-            $("#actualArrivalData"+editButtonKey).text(actualArrival);
-            $("#minsTillArrivalData"+editButtonKey).text(minsTillArrival);
-            console.log(edittedFirstTrainTime);
-       } else {
-            database.ref().child(editButtonKey).update({firstTrainTime: edittedFirstTrainTime});
-            database.ref().child(editButtonKey).update({frequency: edittedFrequency});
-            diffOfCurrentAndFirstTime = moment().diff(moment(edittedFirstTrainTime, "HH:mm"), "minutes");
-            var timeDiffandFreqRemainder = diffOfCurrentAndFirstTime % edittedFrequency;
-            var minsTillArrival = edittedFrequency - timeDiffandFreqRemainder;
-            var actualArrival = moment().add(minsTillArrival, "minutes").format("HH:mm");
-            $("#frequencyData"+editButtonKey).text(edittedFrequency);
-            $("#actualArrivalData"+editButtonKey).text(actualArrival);
-            $("#minsTillArrivalData"+editButtonKey).text(minsTillArrival);
-       }
-      
-       $("#editSubmitButton").removeAttr("editSubmitKey");
-       $(".editForm").hide();
+          if(edittedDestination !== ""){
+               database.ref().child(editButtonKey).update({destination: edittedDestination});
+               $("#destinationData"+editButtonKey).text(edittedDestination);
+          }
+          $("#editSubmitButton").removeAttr("editSubmitKey");
+          $(".editForm").hide();
+          });
 
-    });
-   });
-   
-   
-       
-   
-   
-   
-   
+          $("#editCancelButton").unbind("click").on("click", function(){
+               $(".editForm").hide();
+               $("#editDestination").val("");
+               $("#editFirstTrainTime").val("");
+               $("#editFrequency").val("");
+          });
+          
+     });
+ 
 });
